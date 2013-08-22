72468f2f8033c1511c8d32b04e44c3b5ecfcd5da
Lauri Tirkkonen <lotheac@iki.fi>
2013-08-22 14:10:30 +0000

    add openldap

---
+++ openldap/build.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/bash
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License, Version 1.0 only
+# (the "License").  You may not use this file except in compliance
+# with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+#
+# Copyright 2011-2013 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Use is subject to license terms.
+#
+# Load support functions
+. ../../lib/functions.sh
+
+PROG=openldap
+VER=2.4.36
+VERHUMAN=$VER
+PKG=application/openldap
+SUMMARY="OpenLDAP, community developed LDAP software"
+DESC="$SUMMARY"
+
+BUILD_DEPENDS_IPS='database/bdb'
+
+PREFIX=/opt/openldap
+reset_configure_opts
+CONFIGURE_OPTS="$CONFIGURE_OPTS --localstatedir=/var"
+
+install_manifest() {
+    smfdir=${DESTDIR}/lib/svc/manifest/network/ldap
+    mkdir -p $smfdir
+    cp $SRCDIR/slapd.xml $smfdir
+}
+
+init
+download_source $PROG $PROG $VER
+patch_source
+prep_build
+build
+install_manifest
+make_isa_stub
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:
