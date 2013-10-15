5bfe78de58a5d71c0cf72427f90d7fea5ff5bd8f
Lauri Tirkkonen <lotheac@iki.fi>
2013-10-15 08:11:05 +0000

    tin utf8 default network charset

--- tin/build.sh
+++ tin/build.sh
@@ -48,7 +48,7 @@ make_install() {
     popd >/dev/null
 }
 
-CONFIGURE_OPTS="$CONFIGURE_OPTS --mandir=${PREFIX}/share/man --with-domain-name=/etc/defaultdomain"
+CONFIGURE_OPTS="$CONFIGURE_OPTS --mandir=${PREFIX}/share/man --with-domain-name=/etc/defaultdomain --with-mime-default-charset=UTF-8"
 
 init
 download_source $PROG $PROG $VER
