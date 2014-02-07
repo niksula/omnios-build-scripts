49498a8a8bc151c114082ce1a8c67986c97c30c3
Lauri Tirkkonen <lotheac@iki.fi>
2014-02-07 16:27:33 +0200

    libijs

---
+++ libijs/build.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/bash
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License, Version 1.0 only
+# (the "License").  You may not use this file except in compliance
+# with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+#
+# Copyright 2011-2013 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Use is subject to license terms.
+#
+# Load support functions
+. ../../lib/functions.sh
+
+# XXX ghostscript supposedly includes libijs now, but it doesn't seem to build
+# a shared library of it (there's a TODO note about it in the ghostscript
+# source tree), so let's just use this old version
+
+PROG=ijs
+VER=0.35
+VERHUMAN=$VER
+PKG=library/libijs
+SUMMARY="IJS raster image transport protocol library"
+DESC="$SUMMARY"
+
+init
+download_source $PROG $PROG $VER
+patch_source
+prep_build
+build
+make_isa_stub
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:
