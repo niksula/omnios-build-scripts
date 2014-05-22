7305cbb62d4cb7f7e4b3b8d39395a400e01ea612
Lauri Tirkkonen <lotheac@iki.fi>
2014-05-22 14:14:11 +0300

    collectd 5.4.1

---
+++ collectd/build.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/bash
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License, Version 1.0 only
+# (the "License").  You may not use this file except in compliance
+# with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+#
+# Copyright 2011-2013 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Use is subject to license terms.
+#
+# Load support functions
+. ../../lib/functions.sh
+
+PROG=collectd
+VER=5.4.1
+VERHUMAN=$VER
+PKG=application/collectd
+SUMMARY="system statistics collection daemon"
+DESC="$SUMMARY"
+
+# need autoconf to apply some patches to configure.ac
+BUILD_DEPENDS_IPS='application/rrdtool developer/build/pkg-config developer/build/autoconf'
+
+CONFIGURE_OPTS="$CONFIGURE_OPTS --disable-static"
+
+PKG_CONFIG=${PREFIX}/bin/pkg-config
+export PKG_CONFIG
+
+TAR=gtar
+
+init
+download_source $PROG $PROG $VER
+patch_source
+prep_build
+logcmd autoconf
+build
+make_isa_stub
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:
