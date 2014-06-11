ee1027aa69797c39b7cb6c0aa8247037f68287ef
Lauri Tirkkonen <lotheac@iki.fi>
2014-06-11 10:32:47 +0300

    puppetlabs security fixes
    puppet 3.6.2
    facter 2.0.2
    hiera 1.3.4

--- facter/build.sh
+++ facter/build.sh
@@ -28,7 +28,7 @@
 . ../../lib/functions.sh
 
 PROG=facter
-VER=2.0.1
+VER=2.0.2
 VERHUMAN=$VER
 PKG=application/facter
 SUMMARY="Facter, a system inventory tool"
--- hiera/build.sh
+++ hiera/build.sh
@@ -28,7 +28,7 @@
 . ../../lib/functions.sh
 
 PROG=hiera
-VER=1.3.3
+VER=1.3.4
 VERHUMAN=$VER
 PKG=application/hiera
 SUMMARY="Light weight hierarchical data store"
