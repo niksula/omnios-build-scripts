1f6bc40bb0c7f778ef7cb89b4dddd2bcc4ccabc9
Lauri Tirkkonen <lotheac@iki.fi>
2014-09-22 14:45:03 +0300

    include aspell-en dict with aspell

--- aspell/build.sh
+++ aspell/build.sh
@@ -34,11 +34,52 @@ PKG=library/aspell
 SUMMARY="GNU Aspell spell-checker"
 DESC="$SUMMARY"
 
+DICTPROG=aspell6-en
+DICTVER=7.1-0
+
 CONFIGURE_OPTS="--disable-curses"
+
+save_function make_install make_install_orig
+make_install() {
+    make_install_orig
+    logmsg 'Building dictionary files'
+    pushd $TMPDIR/${DICTPROG}-${DICTVER} >/dev/null
+    orig_path=$PATH
+    # this has a "configure" but it doesn't work the usual way, and we need
+    # aspell to build the dictionary. isa stubs don't exist at this time
+    logmsg "cwd is $(pwd)"
+    if [ "$ISALIST" = "$ISAPART" ]; then
+        logcmd gmake distclean
+        logcmd gmake clean
+        PATH=${orig_path}:${DESTDIR}${PREFIX}/bin/$ISAPART
+        CONFIGURE_OPTS= CONFIGURE_OPTS_32= configure32 || logerr 'configure failed'
+        make_prog32 || logerr 'make failed'
+        logcmd gmake install DESTDIR=$DESTDIR || logerr 'make install failed'
+    else
+        logcmd gmake distclean
+        logcmd gmake clean
+        PATH=${PATH}:${DESTDIR}${PREFIX}/bin/$ISAPART64
+        CONFIGURE_OPTS= CONFIGURE_OPTS_64= configure64 || logerr 'configure failed'
+        make_prog64 || logerr 'make failed'
+        logcmd gmake install DESTDIR=$DESTDIR || logerr 'make install failed'
+    fi
+    PATH=$orig_path
+    unset orig_path
+    popd >/dev/null
+}
+
+get_dict() {
+    logmsg 'Downloading and extracting dictionary source'
+    pushd ${TMPDIR} >/dev/null
+    get_resource ${PROG}/${DICTPROG}-${DICTVER}.tar.bz2 || logerr 'download failed'
+    logcmd extract_archive ${DICTPROG}-${DICTVER}.tar.bz2 || logerr 'extract failed'
+    popd >/dev/null
+}
 
 init
 download_source $PROG $PROG $VER
 patch_source
+get_dict
 prep_build
 build
 make_isa_stub
