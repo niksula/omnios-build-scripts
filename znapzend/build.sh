e40b843db6987be1f06a6d9790ff3c965cac3c89
Lauri Tirkkonen <lotheac@iki.fi>
2014-10-06 15:16:56 +0300

    add znapzend with workaround for https://github.com/oetiker/znapzend/issues/102

---
+++ znapzend/build.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/bash
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License, Version 1.0 only
+# (the "License").  You may not use this file except in compliance
+# with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+#
+# Copyright 2014 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Use is subject to license terms.
+#
+# Load support functions
+. ../../lib/functions.sh
+
+PROG=znapzend
+VER=0.13.0
+VERHUMAN=$VER
+PKG=service/storage/znapzend
+SUMMARY="zfs backup with mbuffer and ssh support"
+DESC="$SUMMARY"
+
+BUILDARCH=64
+BUILD_DEPENDS_IPS='niksula/perl/mojolicious niksula/perl/mojo-ioloop-forkcall'
+PREFIX=${PREFIX}/perl5
+reset_configure_opts
+PATH=${PREFIX}/bin:${PATH}
+CONFIGURE_OPTS="$CONFIGURE_OPTS --bindir=/opt/niksula/bin --sbindir=/opt/niksula/sbin"
+
+init
+download_source $PROG $PROG $VER
+patch_source
+prep_build
+build
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:
