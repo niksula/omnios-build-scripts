9457ffce5d84d21ebe27fc4a8466ebdc09c49750
Lauri Tirkkonen <lotheac@iki.fi>
2014-11-12 16:59:15 +0200

    spamassassin fixes
    Apparently there were some optional modules installed at build time, so
    SpamAssassin couldn't cope without them at runtime. Additionally, make
    it find gpg and work with IPv6 resolvers.

--- perl-modules/Mail-SpamAssassin/build.sh
+++ perl-modules/Mail-SpamAssassin/build.sh
@@ -16,11 +16,12 @@ pkg:/niksula/perl5/HTML-Parser
 pkg:/niksula/perl5/NetAddr-IP
 pkg:/niksula/perl5/Mail-DKIM
 pkg:/niksula/perl5/Net-DNS
-pkg:/niksula/perl5/Digest-SHA1"
-# add some optional modules
-BUILD_DEPENDS_IPS="$BUILD_DEPENDS_IPS
+pkg:/niksula/perl5/Digest-SHA1
 pkg:/niksula/perl5/DB_File
-pkg:/niksula/perl5/Encode-Detect"
+pkg:/niksula/perl5/Encode-Detect
+pkg:/niksula/perl5/HTTP-Date
+pkg:/niksula/perl5/IO-Socket-INET6
+security/gnupg"
 RUN_DEPENDS_IPS="$BUILD_DEPENDS_IPS"
 
 # if we pure_install, we won't get rules files and default configuration
@@ -35,6 +36,9 @@ LOCALSTATEDIR=/opt/niksula/var"
 
 # same problem with make as in NetAddr-IP/build.sh
 NO_PARALLEL_MAKE=1
+
+# gpg needs to be in PATH during build
+PATH=$PATH:/opt/niksula/bin
 
 init
 download_source authors/id/K/KM/KMCGRAIL/SpamAssassin $PROG $VER
