99c0554adb2fff7f418a80168fe62d3d8c08b9ca
Lauri Tirkkonen <lotheac@iki.fi>
2014-11-12 12:47:59 +0200

    correct lib/include paths for perl5/DB_File

--- perl-modules/DB_File/build.sh
+++ perl-modules/DB_File/build.sh
@@ -11,7 +11,8 @@ SUMMARY='Perl5 access to Berkeley DB version 1.x'
 DESC="$SUMMARY"
 
 BUILD_DEPENDS_IPS="=pkg:/niksula/runtime/perl@$PERLVER_MINOR
-pkg:/niksula/runtime/perl@$PERLVER_MINOR"
+pkg:/niksula/runtime/perl@$PERLVER_MINOR
+database/bdb"
 RUN_DEPENDS_IPS="$BUILD_DEPENDS_IPS"
 
 init
