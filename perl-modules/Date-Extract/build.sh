40b6bdcd30cb34e10cd93af777fee52210edf489
Lauri Tirkkonen <lotheac@iki.fi>
2014-11-13 17:40:27 +0200

    replace Alt-Date-Extract-SHARYANTO with Date-Extract
    apparently metacpan maps Date::Extract to the former distribution, but
    rt4's module checks are not ok with that.

--- perl-modules/Date-Extract/build.sh
+++ perl-modules/Date-Extract/build.sh
@@ -3,18 +3,17 @@
 . ../../../lib/functions.sh
 . ../cpan-inc.sh
 
-PROG=Alt-Date-Extract-SHARYANTO
-VER=0.05.01
+PROG=Date-Extract
+VER=0.05
 VERHUMAN="$VER (perl$PERLVER)"
-PKG=niksula/perl5/Alt-Date-Extract-SHARYANTO
-SUMMARY='SHARYANTOs alternate implementation of Date::Extract'
+PKG=niksula/perl5/Date-Extract
+SUMMARY='extract probable dates from strings'
 DESC="$SUMMARY"
 
 BUILD_DEPENDS_IPS="=pkg:/niksula/runtime/perl@$PERLVER_MINOR
 pkg:/niksula/runtime/perl@$PERLVER_MINOR
 pkg:/niksula/perl5/Test-MockTime
 pkg:/niksula/perl5/DateTime-Format-Natural
-pkg:/niksula/perl5/Alt-Base
 pkg:/niksula/perl5/Class-Data-Inheritable"
 RUN_DEPENDS_IPS="$BUILD_DEPENDS_IPS"
 
