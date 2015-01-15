13e7411d191c3307ad75d3af468058fd8785b245
Lauri Tirkkonen <lotheac@iki.fi>
2015-01-15 17:07:55 +0200

    spamassassin: 64bit only, include updated rules files in package

--- perl-modules/Mail-SpamAssassin/build.sh
+++ perl-modules/Mail-SpamAssassin/build.sh
@@ -28,11 +28,16 @@ RUN_DEPENDS_IPS="$BUILD_DEPENDS_IPS"
 make_pure_install() {
     make_install
 }
+# PACKAGING says that LOCALSTATEDIR/spamassassin is used but in reality just
+# LOCALSTATEDIR is used.
 PERL_MAKEFILE_OPTS="$PERL_MAKEFILE_OPTS
 INSTALLVENDORSCRIPT=/opt/niksula/bin
 INSTALLVENDORMAN1DIR=/opt/niksula/share/man/man1
 SYSCONFDIR=/opt/niksula/etc
-LOCALSTATEDIR=/opt/niksula/var"
+DATADIR=/opt/niksula/share/spamassassin
+LOCALSTATEDIR=/opt/niksula/var/spamassassin"
+
+BUILDARCH=64
 
 # same problem with make as in NetAddr-IP/build.sh
 NO_PARALLEL_MAKE=1
@@ -46,12 +51,29 @@ install_manifest() {
     install -m 0444 ${SRCDIR}/spamd.xml ${tgtdir}/spamassassin-spamd.xml || logerr 'manifest install failed'
 }
 
+update_rules() {
+    # because sa-update has -T in the hashbang *and* perl checks that you ran
+    # it with -T in such case, PERL5LIB in the environment won't work. However,
+    # we can cheat by creating a copy of the script without the hashbang;
+    # however, there are further assumptions about paths embedded in the
+    # script, so just hack them to contain DESTDIR as well.
+    sa_update_copy=$(mktemp "${DESTDIR}/sa-update.XXXXXX")
+    (
+    set -e
+    sa_update="${DESTDIR}/opt/niksula/bin/sa-update"
+    sed -e 1d -e "s,/opt/niksula,${DESTDIR}/opt/niksula," "$sa_update" > "$sa_update_copy"
+    PERL5LIB="${DESTDIR}${PREFIX}/lib/vendor_perl/${PERLVER}" ${PREFIX}/bin/perl "$sa_update_copy" --updatedir "${DESTDIR}/opt/niksula/share/spamassassin"
+    ) || logerr 'could not download/install spamassassin rules files'
+    rm -f "${sa_update_copy}"
+}
+
 init
 download_source authors/id/K/KM/KMCGRAIL/SpamAssassin $PROG $VER
 patch_source
 prep_build
 buildperl
 install_manifest
+update_rules
 make_isa_stub
 make_package
 clean_up
