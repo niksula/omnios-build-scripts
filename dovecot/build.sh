2ebce417ea6ef78c795bed09a5dd84f537b22908
Lauri Tirkkonen <lotheac@iki.fi>
2016-10-31 10:59:54 +0200

    update dovecot to 2.2.26.0, pigeonhole to 0.4.16

--- dovecot/build.sh
+++ dovecot/build.sh
@@ -28,7 +28,7 @@
 . ../../lib/functions.sh
 
 PROG=dovecot
-VER=2.2.24
+VER=2.2.26.0
 VERHUMAN=$VER
 PKG=service/network/imap/dovecot
 SUMMARY="dovecot secure IMAP server"
--- pigeonhole/build.sh
+++ pigeonhole/build.sh
@@ -28,7 +28,7 @@
 . ../../lib/functions.sh
 
 PROG=pigeonhole
-VER=0.4.14
+VER=0.4.16
 VERHUMAN=$VER
 PKG=mail/dovecot/pigeonhole
 SUMMARY="Sieve support for Dovecot"
